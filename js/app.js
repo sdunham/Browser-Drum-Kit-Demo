68fe4318a0d52f2caacf2136376f5383d56465b1
sdunham <sdunham@sitecrafting.com>
2016-12-04 17:54:24 -0800

    Added Howler JS library, and started basic DrumKit JS functionality

--- js/app.js
+++ js/app.js
@@ -1,3 +1,56 @@
-// Simple test to load and play an audio file when the page loads
-var testSound = new Audio('./audio/chimes.mp3');
-testSound.play();
+
+var DrumKit = function( drums ){
+	self = this;
+	self.drums = drums;
+	self.curDrum = 0;
+
+	for( var i = 0; i < self.drums.length; i++ ){
+		self.drums[i].howl = new Howl({
+			src: [self.drums[i].src]
+		});
+
+		self.drums[i].howl.once('load', function(){
+			this.play();
+		});
+		console.log( self.drums[i] );
+	}
+};
+
+var myDrumKit = new DrumKit([
+	{
+		id: 1,
+		name: 'Snare',
+		src: 'audio/snare.mp3'
+	},
+	{
+		id: 2,
+		name: 'High Tom',
+		src: 'audio/tomhi.mp3'
+	},
+	{
+		id: 3,
+		name: 'Mid Tom',
+		src: 'audio/tommid.mp3'
+	},
+	{
+		id: 4,
+		name: 'High Hat',
+		src: 'audio/highhat.mp3'
+	},
+	{
+		id: 5,
+		name: 'Cowbell',
+		src: 'audio/cowbell.mp3'
+	},
+	{
+		id: 6,
+		name: 'Bass',
+		src: 'audio/bass.mp3'
+	},
+	{
+		id: 7,
+		name: 'Chimes',
+		src: 'audio/chimes.mp3'
+	}
+
+]);
